c74f7abbdeca76990241897015a9c6084ce4c9ba
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2022-12-11 20:19:39 +0100

    made changes to index file

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ const cloudinary = require("cloudinary").v2;
 
 dotenv.config();
 app.use(express.json());
-api.use(function(req,res,next){
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
-})
+});
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
