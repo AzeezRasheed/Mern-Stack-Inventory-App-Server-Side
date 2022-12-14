e5f1ccaf8eab12299321d3464e2df7aa1dadfc8d
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2022-12-14 15:56:54 +0100

    made changes to env file

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = (url) => {
   mongoose
-    .connect(url,{ useNewUrlParser: true })
+    .connect(url, { useNewUrlParser: true })
     .then(() => console.log("Connected to database"))
     .catch((err) => console.log(err));
 };
 
 module.exports = connectDB;
-
