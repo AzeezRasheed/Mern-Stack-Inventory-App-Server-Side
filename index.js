5036e7636ac4ed19b640cfdece461c4fdb47fef6
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2022-12-16 20:13:27 +0100

    added a mew frontend URL

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // "https://pinvent-app.vercel.app"
+    origin: [
+      "http://localhost:3000",
+      "https://rashie-mern-stack-inventory-app.vercel.app",
+    ],
     credentials: true,
   })
 );
